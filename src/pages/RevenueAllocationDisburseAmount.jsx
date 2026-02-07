de8283378764d8c729373aaae5bdcb79a3accc78
mohinimraut7 <mohinimraut7@gmail.com>
2026-02-07 13:33:24 +0530

    mobile responsive

--- src/pages/RevenueAllocationDisburseAmount.jsx
+++ src/pages/RevenueAllocationDisburseAmount.jsx
@@ -324,12 +324,21 @@ const handleOrderNoChange = async (value) => {
   return (
     <div className="p-6 bg-gray-50 min-h-full">
 
-      <button
+      {/* <button
         onClick={() => setOpenAddActivityModal(true)}
         className="px-4 py-2 bg-blue-600 text-white rounded-lg"
       >
         + Add Disbursement
-      </button>
+      </button> */}
+      <div className="flex justify-end">
+  <button
+    onClick={() => setOpenAddActivityModal(true)}
+    className="px-4 py-2 bg-blue-600 text-white rounded-lg"
+  >
+    + Add Disbursement
+  </button>
+</div>
+
 
 
 <div className="bg-white shadow rounded-xl p-3 mt-4 flex flex-wrap sm:flex-nowrap sm:items-center gap-3">
